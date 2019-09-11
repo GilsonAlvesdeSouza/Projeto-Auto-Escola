b3d9c4d84da872f31f69aac2474db0e821a377a4
Gilson Alves <gilsonalves6@icloud.com>
2019-09-11 10:29:48 -0300

    criação da classe entity

---
+++ projetoautoescola/src/br/com/project/listener/ContextLoaderListenerAutoEcolaUtils.java
@@ -0,0 +1,47 @@
+package br.com.project.listener;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ApplicationScoped;
+
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+/**
+ * Classe que carrega as configurações do spring
+ * 
+ * @author gilsonalves
+ *
+ */
+@ApplicationScoped
+public class ContextLoaderListenerAutoEcolaUtils extends ContextLoaderListener implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static WebApplicationContext getWAC() {
+		return WebApplicationContextUtils
+				.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());
+	}
+
+	/**
+	 * Método que carrega um objeto pelo id
+	 * 
+	 * @param IdNomeBean
+	 * @return
+	 */
+	public static Object getBean(String IdNomeBean) {
+		return getWAC().getBean(IdNomeBean);
+	}
+
+	/**
+	 * Método que carrega uma classe pelo nome da classe
+	 * 
+	 * @param classe
+	 * @return
+	 */
+	public static Object getBean(Class<?> classe) {
+		return getWAC().getBean(classe);
+	}
+
+}
